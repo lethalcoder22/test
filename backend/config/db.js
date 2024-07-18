5c988806aa8b5c39a79f444b27c2ba99181ceef4
nikunj <125471605+lethalcoder22@users.noreply.github.com>
2024-07-18 19:35:35 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/notes-app", {
+    await mongoose.connect("mongodb+srv://nikunj:pure1@gs@cluster0.rfa4cle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
